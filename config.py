3628f039470b6208066e4554575854d06c421fc3
tinowlh <tinowhuang@gmail.com>
2020-12-17 11:02:35 +0800

    add modelname and stock price date

--- config.py
+++ config.py
@@ -5,4 +5,18 @@ n_test = 10
 n_epochs = 20
 n_batch = 32
 n_neurons = 8
-n_runs = 1  # set n_runs > 1 if random seed not used at first  
+n_runs = 1  # set n_runs > 1 if random seed not used at first 
+
+# model saving
+model_name = 'lstm_v1.0'
+scaler_name = 'scaler_v1.0'
+
+# data colloection
+stockp_start_yr = 2018
+stockp_start_mn = 12
+stockp_start_d = 1
+
+stockp_end_yr = 2020
+stockp_end_mn = 12
+stockp_end_d = 15
+
